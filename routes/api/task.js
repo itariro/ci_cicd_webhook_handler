769f938c2105eaa89956cba80b5e48a3b5b36a5e
Tariro S Tobaiwa <tobaiwa@gmail.com>
2022-12-22 11:59:33 +0000

    added a user management module

--- routes/api/task.js
+++ routes/api/task.js
@@ -9,6 +9,7 @@ const {
 	readMultipleAllResourceGeneric,
 } = require("../../services/db_client");
 const { getAllTasks } = require("../../services/task_manager");
+const { registerUserGeneric } = require("../../services/user_manager");
 
 /* LIST all tasks */
 router.get("/:systemid", async function (req, res, next) {
@@ -44,26 +45,36 @@ router.get("/:systemid", async function (req, res, next) {
 /* POST NEW task */
 router.post("/:systemid", async function (req, res, next) {
 	try {
-		createResourceGeneric(
-			"task",
-			{
-				systemid: req.params.systemid,
-				uuid: uuidv4(),
-				user_mobile: req.body.user,
-				query: req.body.query,
-			},
-			function (err, result) {
-				if (err) {
-					res.status(400).json({ error: true, message: err.message });
+
+		registerUserGeneric({ mobile_number: req.body.user, status: 1, balance: 2.00 }, // each new user gets £2 worth of points : CAC
+			function (user_err, user_result) {
+				if (user_err) {
+					res.status(400).json({ error: true, message: user_err.message });
 					return;
 				} else {
-					result.error
-						? res.status(400).json(result)
-						: res.status(200).json(result);
+					user_result.error
+						? res.status(400).json(user_result)
+						: createResourceGeneric(
+							"task",
+							{
+								systemid: req.params.systemid,
+								uuid: uuidv4(),
+								user_mobile: req.body.user,
+								query: req.body.query,
+							},
+							function (task_err, task_result) {
+								if (task_err) {
+									res.status(400).json({ error: true, message: task_err.message });
+									return;
+								} else {
+									res.status(task_result.error ? 400 : 200).json({ user: user_result, task: task_result });
+									return;
+								}
+							}
+						);
 					return;
 				}
-			}
-		);
+			});
 	} catch (error) {
 		console.error(`error while sending message `, error);
 		next(error);
