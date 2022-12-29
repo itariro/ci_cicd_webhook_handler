93c353247aea73669207a4892e96cfc7e783e960
Tariro S Tobaiwa <tobaiwa@gmail.com>
2022-12-29 17:05:35 +0000

    housekeeping

--- routes/api/catalogue.js
+++ routes/api/catalogue.js
@@ -4,10 +4,9 @@ const {
 	createResourceGeneric,
 } = require("../../services/db_client");
 const { getAllTasks } = require("../../services/task_manager");
-const { registerUserGeneric } = require("../../services/user_manager");
 
-/* LIST all tasks */
-router.get("/:systemid", async function (req, res, next) {
+/* LIST all from a specific user */
+router.get("/user/:user_mobile", async function (req, res, next) {
 	try {
 		const pendingTasks = await getAllTasks();
 		pendingTasks.error
@@ -21,43 +20,52 @@ router.get("/:systemid", async function (req, res, next) {
 	}
 });
 
-/* POST NEW task */
-router.post("/:systemid", async function (req, res, next) {
+/* LIST all based on a task */
+router.get("/task/:task_uuid", async function (req, res, next) {
 	try {
-		const { v4: uuidv4 } = require('uuid');
-		registerUserGeneric({ mobile_number: req.body.user, status: 1, balance: 2.00 }, // each new user gets £2 worth of points : CAC
-			function (user_err, user_result) {
-				if (user_err) {
-					res.status(400).json({ error: true, message: user_err.message });
-					return;
-				} else {
-					user_result.error
-						? res.status(400).json(user_result)
-						: createResourceGeneric(
-							"task",
-							{
-								systemid: req.params.systemid,
-								uuid: uuidv4(),
-								user_mobile: req.body.user,
-								query: req.body.query,
-							},
-							function (task_err, task_result) {
-								if (task_err) {
-									res.status(400).json({ error: true, message: task_err.message });
-									return;
-								} else {
-									res.status(task_result.error ? 400 : 200).json({ user: user_result, task: task_result });
-									return;
-								}
-							}
-						);
-					return;
-				}
-			});
+		const pendingTasks = await getAllTasks();
+		pendingTasks.error
+			? res.status(400).json(pendingTasks)
+			: res.status(200).json(pendingTasks);
+		return;
 	} catch (error) {
-		console.error(`error while sending message `, error);
+		console.error(`error while fetching task list`, error);
+		res.status(400).json({ error: true, message: error });
 		next(error);
 	}
 });
+
+async function getAllCatalogueItems(siftFilter) {
+	try {
+		let tableModel = global.CURRENT_MODELS.find(
+			(tableProperties) => tableProperties.table_name === "user_mobile_catalogue"
+		);
+		if (tableModel != null) {
+			const pendingTasks = await tableModel.model_name.findAll({
+				logging: false,
+				where: {
+					[Op.and]: [{ attempts: [0, 1, 2] }, { status: 0 }, { in_queue: 0 }],
+				},
+			});
+			// process the outstanding TASKS HERE
+			return {
+				error: false,
+				data: pendingTasks,
+			};
+		} else {
+			console.log("resource does not exist");
+			return {
+				error: true,
+				message: "resource does not exist",
+			};
+		}
+	} catch (error) {
+		console.log(error);
+		return {
+			error: true,
+			message: error,
+		};
+	}
+}
 
 module.exports = router;
